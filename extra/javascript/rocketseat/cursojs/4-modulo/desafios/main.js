a1c93dea4ca2c6933f96ec5299192992bd93ae60
Emanuel <emanuel.duarte.maia@gmail.com>
2020-09-16 17:24:03 -0300

    Exercicio 2 e 3 #1 | consumindo API GitHub

--- extra/javascript/rocketseat/cursojs/4-modulo/desafios/main.js
+++ extra/javascript/rocketseat/cursojs/4-modulo/desafios/main.js
@@ -1,22 +1,50 @@
+
+// Exercício 1
 function checaIdade(idade) {
   return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'idade')
-    xhr.send(null);
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(xhr.responseURL);
-        } else {
-          reject('Erro na requisição');
-        }
+    setTimeout(function () {
+      if (idade >= 18) {
+        resolve();
+      } else {
+        reject();
       }
-    }
+    }, 2000)
   });
 }
 
-checaIdade()
+checaIdade(20)
   .then(function (response) {
-    console.log(response);
+    console.log('Maior que 18');
   })
+  .catch(function (error) {
+    console.warn('Menor que 18');
+  })
+
+// Exercício 2 e 3
+// Button
+var ul = document.querySelector('ul');
+document.querySelector('#listDataUser').onclick = () => {
+  var userName = document.querySelector('input[name=user]');
+  ul.innerHTML = '';
+
+  axios.get(`https://api.github.com/users/${userName.value}/repos`)
+    // .interceptors.request.use(function (config) {
+    //   return config;
+    // })
+    .then(function (response) {
+      response.data.forEach(element => {
+        var li = document.createElement('li');
+        var textFork = element.fork ? ' (Fork)' : '';
+        var text = document.createTextNode(element.name + textFork);
+        li.appendChild(text);
+        ul.appendChild(li);
+      })
+    })
+    .catch(function (error) {
+      if (error.response.status == 404) {
+        alert('Usuário inexistente');
+      } else {
+        console.warn('Erro na requisição');
+      }
+    })
+}
