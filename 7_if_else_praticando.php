73f404fd106d7993cb737be2222c454738705517
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-08-26 14:36:11 -0300

    feat: if/else pt 4

---
+++ 7_if_else_praticando.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style.css">
+  <title>E-commerce</title>
+  <style>
+    .rtx {
+      height: 320px;
+      background-attachment: fixed;
+      background-image: url(https://images.kabum.com.br/produtos/fotos/128377/placa-de-video-evga-nvidia-geforce-rtx-3090-24gb-gddr6x-24g-p5-3987-kr_1600785254_gg.jpg);
+    }
+
+    main {
+      padding: 10rem;
+      align-items: center;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="rtx"></div>
+  <?php
+  $usuario_possui_cartao_loja = true;
+  $valor_compra = 17750;
+
+  $valor_frete = 50;
+  $recebeu_desconto_frete = false;
+
+  if ($usuario_possui_cartao_loja && $valor_compra >= 100) {
+    $valor_frete = 0;
+    $recebeu_desconto_frete = true;
+  }
+  ?>
+
+  <main>
+    <h1>Detalhes do pedido</h1>
+    <p><strong>Possui cartão da loja?</strong> <?= ($usuario_possui_cartao_loja) ? 'Sim' : 'Não';  ?></p>
+
+    <p><strong>Valor da compra:</strong> R$<?= number_format($valor_compra, 2, ',', ''); ?></p>
+
+    <p><strong>Recebeu desconto no frete?</strong> <?= ($recebeu_desconto_frete) ? 'Sim' : 'Não';  ?></p>
+  </main>
+
+  <div class="rtx"></div>
+</body>
+
+</html>
